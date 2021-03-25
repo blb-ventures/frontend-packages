3e78f3a02ee32699946c6e39f81c65818974f851
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2021-03-25 15:37:27 -0300

    Names jobs commands Adds node resolver to try fixing eslint inside the github actions

--- packages/react-components/.eslintrc.js
+++ packages/react-components/.eslintrc.js
@@ -34,10 +34,10 @@ module.exports = {
     // 'graphql',
   ],
   settings: {
-    'react': {
+    react: {
       version: 'detect',
     },
-    'linkComponents': [
+    linkComponents: [
       {
         name: 'Link',
         linkAttribute: 'href',
@@ -51,6 +51,10 @@ module.exports = {
       typescript: {
         alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
         project: path.resolve(__dirname, './tsconfig.json'),
+      },
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
@@ -146,9 +150,9 @@ module.exports = {
     '@typescript-eslint/unbound-method': 'off',
     '@typescript-eslint/unified-signatures': 'error',
     'arrow-parens': ['warn', 'as-needed'],
-    'complexity': 'off',
+    complexity: 'off',
     'constructor-super': 'error',
-    'eqeqeq': ['error', 'smart'],
+    eqeqeq: ['error', 'smart'],
     'guard-for-in': 'error',
     'id-blacklist': [
       'error',
@@ -206,7 +210,7 @@ module.exports = {
     'one-var': ['error', 'never'],
     'prefer-arrow-functions/prefer-arrow-functions': 'error',
     'prefer-const': 'error',
-    'radix': 'error',
+    radix: 'error',
     'require-await': 'off',
     'spaced-comment': [
       'error',
